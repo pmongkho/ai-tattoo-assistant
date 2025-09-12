ed61f8afda9bb44772936cf9cf713b17706e9d21
Phongsavanh Mongkhonvilay <pmongkho12@gmail.com>
2025-09-12 11:46:18 -0500

    feat: add Meta webhook for Messenger

---
+++ dotnet-server/_Models/MetaWebhookModels.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+
+namespace DotNet.Models
+{
+    public class MetaEvent
+    {
+        public List<MetaEntry> Entry { get; set; } = new();
+    }
+
+    public class MetaEntry
+    {
+        public List<MetaMessaging> Messaging { get; set; } = new();
+    }
+
+    public class MetaMessaging
+    {
+        public MetaSender Sender { get; set; } = new();
+        public MetaMessage Message { get; set; } = new();
+    }
+
+    public class MetaSender
+    {
+        public string Id { get; set; } = string.Empty;
+    }
+
+    public class MetaMessage
+    {
+        public string Text { get; set; } = string.Empty;
+    }
+}
